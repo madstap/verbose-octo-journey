f5404a3881c40c7277e145f0211e71f9623b7e48
Rodrigo de Almeida Garcia <rodrigo.garcia@paygo.corp>
2019-10-08 10:32:00 -0300

    Organizando

--- src/dojo/core.clj
+++ src/dojo/core.clj
@@ -1,42 +1,39 @@
 (ns dojo.core
   (:require
-   [io.pedestal.http :as http]
-   [io.pedestal.http.route :as route]
-   [com.stuartsierra.component :as component]
-   [datomic.api :as d]))
-
-(defn respond-hello [request]
-  {:status 200, :body "Hello, world!"})
+    [com.stuartsierra.component :as component]
+    [datomic.api :as d]))
 
 (def uri "datomic:mem:/dojo")
 
 (def schema
-  [{:db/ident :show/name
-    :db/valueType :db.type/string
+  [{:db/ident       :show/name
+    :db/valueType   :db.type/string
     :db/cardinality :db.cardinality/one}
 
-   {:db/ident :show/date
-    :db/valueType :db.type/instant
+   {:db/ident       :show/date
+    :db/valueType   :db.type/instant
     :db/cardinality :db.cardinality/one}
 
-   {:db/ident :show/id
-    :db/valueType :db.type/long
+   {:db/ident       :show/id
+    :db/valueType   :db.type/long
     :db/cardinality :db.cardinality/one
-    :db/unique :db.unique/identity}])
+    :db/unique      :db.unique/identity}])
 
 (defrecord Datomic [uri schema conn]
   component/Lifecycle
   (start [this]
     (d/create-database uri)
-    (assoc this :conn (d/connect uri)))
+    (let [conn (d/connect uri)]
+      (d/transact conn schema)
+      (assoc this :conn conn)))
   (stop [this]
     (d/delete-database uri)))
 
-(def system (atom nil))
+(defonce system (atom nil))
 
 (def system-map
   (component/system-map
-   :db (map->Datomic {:uri uri, :schema schema})))
+    :db (map->Datomic {:uri uri, :schema schema})))
 
 (defn start []
   (reset! system (component/start-system system-map)))
@@ -44,61 +41,45 @@
 (defn stop []
   (swap! system component/stop-system))
 
+
 (comment
+  ;; dev
 
-  @thing
-
-  (reset! thing 1)
-
-  (swap! thing - 10)
-
-  (- atual 10)
-
-  (:conn (:db @system))
+  (def conn (:conn (:db @system)))
 
   (start)
 
   (stop)
 
-
-  (d/delete-database uri)
+  (deref system)
 
   (d/transact conn schema)
 
   (d/transact conn
               [{;; :show/id 1
                 :show/name "Rock in Rio"
-                :show/date #inst "2019-01-01"
-                :foo/bar 123}])
+                :show/date #inst "2019-01-01"}])
+  )
 
+(comment
+  ;; query
 
-  [id attr value tx valid?]
-
-  [[123 :show/name "Rock in Rio" 456 true]
-   [123 :show/date #inst "2019-01-01" 456 true]
-   ;; [456 :db/txInstant #inst "" ,,, ,,,]
-   [123 :show/name "asdasd" 678 ]]
-
-
-  (d/q '{:find [?e]
-         :in [$ ?name]
+  (d/q '{:find  [?e]
+         :in    [$ ?name]
          :where [[?e :show/name ?name]]}
        (-> (d/db conn)
-           (d/as-of #inst "2019-01-01"))
+           #_(d/as-of #inst "2019-01-01"))
        "Rock in Rio")
 
-  (d/q '{:find [?e1 ?e2]
-         :in [$ ?name]
+  (d/q '{:find  [?e1 ?e2]
+         :in    [$ ?name]
          :where [[?e1 :show/name ?name]
                  [?e2 :show/name ?name]]}
        (d/db conn)
        "Rock in Rio")
 
-
-  (d/q '{:find [(pull ?e [:show/name :show/id])]
-         :in [$ $other-db ?name]
+  (d/q '{:find  [(pull ?e [:show/name :show/id])]
+         :in    [$ $other-db ?name]
          :where [[?e :show/name ?name]]}
        (d/db conn)
-       "Rock in Rio")
-
-  )
+       "Rock in Rio"))
